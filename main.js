284abdba59eefb6c926711c261ed5bcc5eec6932
Alfredo Aguiar <77788945+bealfredo@users.noreply.github.com>
2021-10-24 00:15:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+    github: 'bealfredo',
+    youtube: 'alfredodesouza',
+    instagram: 'alnerve_',
+    facebook: 'alfredo.souza.1884',
+    twitter: 'NettoSouzas'
+}
+
+function changeSocialMediaLink() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `http://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLink()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url).then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name        //.textContent == substitui tudo
+        userBio.textContent = data.bio
+        userLink.href = data.html_url           //.href == muda só o link
+        userImage.src = data.avatar_url         //.src == muda a foto
+        userLogin.textContent = data.login
+    })
+
+}
+
+getGitHubProfileInfos()
